e89ccafb11c627dbf788b38d7885bc3665d68230
Kit Bolourchi <74569356+KitBolourchi@users.noreply.github.com>
2023-03-15 21:03:43 +0000

    Feat: added view-controller-model arch

---
+++ src/main/java/com/example/go/Model/Square.java
@@ -0,0 +1,4 @@
+package com.example.go.Model;
+
+public class Square {
+}
---
+++ src/main/java/com/example/go/View/GoBoard.java
@@ -0,0 +1,20 @@
+package com.example.go.View;
+
+import javafx.geometry.Insets;
+import javafx.scene.layout.GridPane;
+
+import com.example.go.Model.Square;
+
+import java.util.ArrayList;
+
+public class GoBoard {
+
+    GridPane goBoard;
+    String theme;
+    ArrayList<Square> sqaures = new ArrayList<Square>();
+
+
+    public void GoBoard() {
+
+    }
+}
